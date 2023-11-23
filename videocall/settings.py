897337a143243b7fac2e055b568b6c6240675e34
Le Quang Nhat <lqnhat136@gmail.com>
2023-11-23 23:23:22 +0700

    allow host

--- videocall/settings.py
+++ videocall/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9ndce&l@l+tiq0&hpd^9p!xki()!dka-l+ad+$cn)48v-%(ybr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app",".now.sh","rtcvideocall.pythonanywhere.com", "django-videocall.herokuapp.com", "localhost",'127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
