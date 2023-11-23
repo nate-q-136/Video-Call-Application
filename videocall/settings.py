1901afe79c5d1cbc4c73042cb01b166899015c13
Le Quang Nhat <lqnhat136@gmail.com>
2023-11-23 23:25:28 +0700

    customized commit message

--- videocall/settings.py
+++ videocall/settings.py
@@ -140,8 +140,8 @@ STATIC_URL = '/static/'
 # ]
 
 # For Deployments
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
